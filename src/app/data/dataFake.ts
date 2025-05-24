bdd62aba32f4951effc242362b151c8fe6012d03
ViniciusKLima <viniciuskls2006@gmail.com>
2025-05-24 12:28:32 -0300

    Adicionando projeto Angular Blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,32 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "VINGADORES 5 ADIADO!",
+    "description": "O filme Vingadores 5, que estava previsto para ser lançado em 2025, foi adiado para 2026. A decisão foi tomada pela Marvel Studios devido a mudanças na produção e no cronograma de filmagens. O filme, que será dirigido por Destin Daniel Cretton, contará com o retorno de vários personagens icônicos do universo Marvel, incluindo os Vingadores originais e novos heróis.  Os fãs estão ansiosos para ver como a história se desenrolará e quais novos desafios os heróis enfrentarão.",
+    "photoCover":"https://img.tribunahoje.com/aiCQvPfrHO_de9LWcXXru0Yh61Y=/840x520/smart/s3.tribunahoje.com/uploads/imagens/vingadoresdoomsday1.jpeg"
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "id": "2",
+    "title": "THUNDERBOLTS* atual sucesso da Marvel",
+    "photoCover": "https://cinebuzz.com.br/media/_versions/2025/04/thunderbolts-da-marvel-nao-e-nada-do-que-parece-e-isso-e-maravilhoso-leia-a-critica-1_widelg.jpg",
+    "description": "A série 'Thunderbolts' da Marvel, que estreou recentemente, tem sido um sucesso de crítica e público. A trama gira em torno de um grupo de anti-heróis que se unem para enfrentar ameaças globais, trazendo uma nova perspectiva ao universo Marvel."
+  },
+  {
+    "id":"3",
+    "title": "Qual o futuro da Agatha Harkness?",
+    "description": "A série 'Agatha: Darkhold Diaries' está programada para estrear em 2024 e promete explorar o passado e o futuro da personagem Agatha Harkness, interpretada por Kathryn Hahn.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdQhqlP_X8qofBPKTdVOjArEljGmRjpHsF_g&s"
+  },
+  {
+    "id":"4",
+    "title": "Série do Demolidor é um sucesso!",
+    "description": "A nova série do Demolidor, intitulada 'Demolidor: Born Again', tem sido um grande sucesso entre os fãs. A série traz de volta Charlie Cox como Matt Murdock e promete explorar mais a fundo o lado sombrio do personagem.",
+    "photoCover":"https://i.ytimg.com/vi/ocsCQQj7GaM/maxresdefault.jpg"
+  },
+  {
+    "id":"5",
+    "title": "RIVALS! Novo jogo de tabuleiro da Marvel",
+    "description": "O jogo de tabuleiro 'Marvel Rivals' é um emocionante jogo de estratégia que coloca os jogadores no papel de super-heróis e vilões do universo Marvel.",
+    "photoCover": "https://comandogeek.com.br/blog/storage/2024/11/marvel-rivals-1920x1080-16086.jpeg"
   }
 ]
